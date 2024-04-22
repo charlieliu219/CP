2646fba1afdd2eb6fa651ef1149452ed998b7d9b
charlieliu219 <125988414+charlieliu219@users.noreply.github.com>
2024-04-22 14:24:35 +0800

    io

---
+++ source/casting.cpp
@@ -0,0 +1,38 @@
+// #include <bits/stdc++.h>
+#include <iostream>   
+#include <vector>     
+#include <algorithm>  // For algorithms like std::sort
+
+#define rep(a, b)   for(int a = 0; a < (b); ++a)
+#define all(a)      (a).begin(),(a).end()
+// #define endl        '\n'
+
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+
+vector<int> transmutate(string colors){
+    vector<int> indices;
+    cout << "YES" << endl;
+    return indices;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int t; cin>>t;
+    string colors;
+    while (t--) {
+        cin >> colors; 
+        char target = colors[0];
+        cout << colors << endl;
+        cout << target << endl;
+    }
+
+    return 0;
+}
+
+
+
--- source/example.cpp
+++ source/example.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 
 #define rep(a, b)   for(int a = 0; a < (b); ++a)
